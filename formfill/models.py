30b90506bd1f29d900613a71e74c257269494320
krishnanpandya007 <krishnanpandya06@gmail.com>
2023-05-29 16:10:27 +0530

    Added model.py

--- formfill/models.py
+++ formfill/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
